45472a44d590ebc8b2f4af1cbd4adffa671c172f
jessssssssssss <80199144+kuka666@users.noreply.github.com>
2022-03-04 06:29:47 +0600

    Update urls.py

--- cnnProject/urls.py
+++ cnnProject/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='homepage'),
     path('predictImage', views.predictImage, name="predictImage")
-    url(r'^media/(?P<path>.)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 
